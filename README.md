854ff208d70e9beadc878b30dc00139bca9c21d3
ConmineLP <conminelp321@gmail.com>
2022-01-03 00:52:38 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# Sneaky Tree Growing
- Sneak to gorw trees!
+<p><br /><img src="https://i.imgur.com/KoTKu8x.png" alt="Ore Duplicators 2 Logo" width="174" height="174" /></p>
+#Sneaky Tree Growing
+<p>Sneak to grow Trees!</p>
+
+#####Like the project? Leave a 💖Follow on this Mod!
+<hr>
+##Downloads
+You can find downloads here on CurseForge.
+There is no alternative download links/pages that are offical maintained by Qubik Studios when they are not shown here.
+<hr>
+##Features
+Sneak in a 6x6 area to apply Saplings a Bonemeal effect.
+You can change the radius and chance of this effect in the Config
+
+You can also enable certain functions like a mode where it also apply the Bonemeal effect to seeds/crops. (Build in only works with Vanilla Minecraft seeds)
+
+#####Custom Tags
+In the newest version you can now add tags from mods that are not supportet. As example you can add manually support for Dynamic tree's.
+Just go for that in the config and add your faverite mod tag in the section: "Custom-Tags = []"
+An example how you use the custom tag system is in the Config.
+#####Config
+This is the unconfigured Config you will find for this mod.
+```toml
+
+[SneakyTreeGrowing]
+	#Increasing this value will change the area-of-effect from the mod
+	#Default value: 6
+	#Range: 1 ~ 1000
+	Tree-Meal-Radius = 6
+	#Changing this value will change the chance if a bonemeal effect gets applied or not
+	#Default value: 15
+	#Range: 1 ~ 100
+	Tree-Meal-Chance = 15
+
+	[SneakyTreeGrowing.Crop-Settings]
+		#Changing this value to true will allow the mod to apply the bonemeal effect to crops like wheat and potato. Only works on Vannila plants
+		#Default value: false
+		Crop-Meal-Allowed = false
+		#Increasing this value will change the area-of-effect from the crop meal effect
+		#Default value: 6
+		#Range: 1 ~ 25
+		Crop-Meal-Radius = 6
+		#Changing this value will change the chance if a bonemeal effect gets applied to crops or not
+		#Default value: 5
+		#Range: 1 ~ 50
+		Crop-Meal-Chance = 5
+
+	[SneakyTreeGrowing.Custom-Tag]
+		#Enable Custom tag support for the mod. (WIP)
+		#Default value: false
+		Enable-Custom-Tags = false
+		#All values added in this list will result in support for the bonemeal effect.
+		#Example: ["forge:seeds", "minecraft:crops"]
+		Custom-Tags = []
+		#Increasing this value will change the area-of-effect from the custom-tag meal effect
+		#Default Value: 6
+		#Range: 1 ~ 1000
+		Custom-Tags-Meal-Radius = 6
+		#Changing this value will change the chance if a bonemeal effect gets applied to custom tags or not
+		#Default Value: 15
+		#Range: 1 ~ 1000
+		Custom-Tags-Meal-Chance = 15
+```
+<hr>
+##Note on Modpack creators and Disseminators
+You can use this mod for free in your Modpack. Just make sure to credit this mod.
+If you want to republish this mod on a other platform you have to join our Discord and ask the Developer.
+
+Reselling of this mod is prohibited! If you bought this mod please join our Discord and report the person who was selling this to you.
+<hr>
+##Need help? Join our Discord!
+<p><br /><a href="http://discord.qubik-studios.net" target="_blank" rel="noopener noreferrer"><img src="https://discordapp.com/api/guilds/759767022916599808/embed.png?style=banner3" alt="Discord" width="320" height="140" /></a></p>
+
+######Have fun!<br>A project maintained by
+<p><img src="https://qubik-studios.net/wp-content/uploads/2021/10/QUBIK-STUDIOS-BANNER-DARKMODE.png" alt="Qubik Studios" width="294" height="114" /></p>
---
+++ src/main/java/net/qubikstudios/sneakytreegrowing/functions/Treemealer.java
@@ -0,0 +1,92 @@
+package net.qubikstudios.sneakytreegrowing.functions;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BoneMealItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.qubikstudios.sneakytreegrowing.config.MainConfig;
+
+import java.util.Objects;
+
+public class Treemealer {
+
+    public static void execute(LevelAccessor world, Entity entity) {
+        if (entity == null) return;
+        double sx;
+        double sy;
+        double sz;
+        double value_neg;
+        double value_pos;
+        double value_raw = 0;
+        double chance_raw = 0;
+        double chance;
+        if (MainConfig.COMMON.treeMealRadius.get() < 1) {
+            if (world instanceof Level) {
+                value_raw = 2;
+            }
+        } else {
+            value_raw = MainConfig.COMMON.treeMealRadius.get();
+        }
+        if (MainConfig.COMMON.treeMealChance.get() > 100) {
+            if (world instanceof Level) {
+                chance_raw = 100;
+            }
+        } else {
+            chance_raw = MainConfig.COMMON.treeMealChance.get();
+        }
+        chance = chance_raw / 100;
+        value_pos = value_raw;
+        value_neg = value_raw / (-2);
+        if (!(new Object() {
+            public boolean checkGamemode(Entity _ent) {
+                if (_ent instanceof ServerPlayer _serverPlayer) {
+                    return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SPECTATOR;
+                } else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+                    return Objects.requireNonNull(Minecraft.getInstance().getConnection()).getPlayerInfo(_player.getGameProfile().getId()) != null && Objects.requireNonNull(Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId())).getGameMode() == GameType.SPECTATOR;
+                }
+                return false;
+            }
+        }.checkGamemode(entity))) {
+            if (entity.isShiftKeyDown()) {
+                if (Math.random() < chance && entity.getPersistentData().getDouble("isSneaked") == 0) {
+                    entity.getPersistentData().putDouble("isSneaked", 1);
+                    sx = value_neg;
+                    for (int index0 = 0; index0 < (int) (value_pos); index0++) {
+                        sy = value_neg;
+                        for (int index1 = 0; index1 < (int) (value_pos); index1++) {
+                            sz = value_neg;
+                            for (int index2 = 0; index2 < (int) (value_pos); index2++) {
+                                if (BlockTags.getAllTags().getTagOrEmpty(new ResourceLocation("minecraft:saplings")).contains((world.getBlockState(new BlockPos((int) (entity.getX() + sx), (int) (entity.getY() + sy), (int) (entity.getZ() + sz)))).getBlock()) || BlockTags.getAllTags().getTagOrEmpty(new ResourceLocation("forge:sneakytreegrowingallowed")).contains((world.getBlockState(new BlockPos((int) (entity.getX() + sx), (int) (entity.getY() + sy), (int) (entity.getZ() + sz)))).getBlock())) {
+                                    if (world instanceof Level _level) {
+                                        if (BoneMealItem.growCrop(new ItemStack(Items.BONE_MEAL), _level, new BlockPos((int) (entity.getX() + sx), (int) (entity.getY() + sy), (int) (entity.getZ() + sz))) || BoneMealItem.growWaterPlant(new ItemStack(Items.BONE_MEAL), _level, new BlockPos((int) (entity.getX() + sx), (int) (entity.getY() + sy), (int) (entity.getZ() + sz)), null)) {
+                                            if (!_level.isClientSide())
+                                                _level.levelEvent(2005, new BlockPos((int) (entity.getX() + sx), (int) (entity.getY() + sy), (int) (entity.getZ() + sz)), 0);
+                                        }
+                                    }
+                                    for (int index3 = 0; index3 < 3; index3++) {
+                                        world.addParticle(ParticleTypes.HAPPY_VILLAGER, (entity.getX() + sx), (entity.getY() + sy), (entity.getZ() + sz), 0, 1, 0);
+                                    }
+                                }
+                                sz = sz + 1;
+                            }
+                            sy = sy + 1;
+                        }
+                        sx = sx + 1;
+                    }
+                }
+            } else if (!entity.isShiftKeyDown()) {
+                entity.getPersistentData().putDouble("isSneaked", 0);
+            }
+        }
+    }
+}
