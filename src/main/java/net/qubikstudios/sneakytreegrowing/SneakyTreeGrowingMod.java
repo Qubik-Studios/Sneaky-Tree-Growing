2c487922a861777fbc91f5928ffbca0f94a6c0e2
ConmineLP <conminelp321@gmail.com>
2022-01-17 17:37:50 +0100

    Update SneakyTreeGrowingMod.java

--- src/main/java/net/qubikstudios/sneakytreegrowing/SneakyTreeGrowingMod.java
+++ src/main/java/net/qubikstudios/sneakytreegrowing/SneakyTreeGrowingMod.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 @Mod("sneaky_tree_growing")
 public class SneakyTreeGrowingMod {
 
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.18.1-1.7.1";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public SneakyTreeGrowingMod() {
