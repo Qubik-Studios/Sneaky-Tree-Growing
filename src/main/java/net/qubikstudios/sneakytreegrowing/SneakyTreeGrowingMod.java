701ae1acc0e78cab9dac5ea6f43275d92269f6be
ConmineLP <conminelp321@gmail.com>
2022-05-03 18:28:59 +0200

    Changed Version
    Pushed version to 1.18.2-1.8.0

--- src/main/java/net/qubikstudios/sneakytreegrowing/SneakyTreeGrowingMod.java
+++ src/main/java/net/qubikstudios/sneakytreegrowing/SneakyTreeGrowingMod.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 @Mod("sneaky_tree_growing")
 public class SneakyTreeGrowingMod {
 
-    public static final String VERSION = "1.18.2-1.7.2";
+    public static final String VERSION = "1.18.2-1.8.0";
     private static final Logger LOGGER = LogManager.getLogger();
 
     public SneakyTreeGrowingMod() {
